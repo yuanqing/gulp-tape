50f3c93e08ed7b2ac39b96bac5b64d895218773c
Martin Brennan <martin.brennan@csnet.net.au>
2015-09-01 11:33:03 +1000

    added check to see if outputStream is a function and to initialize it if it is. also pipe to process.stdout because this is required in pretty much every reporter

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ var PLUGIN_NAME = 'gulp-tape';
 
 var gulpTape = function(opts) {
   opts = opts || {};
+
   var outputStream = opts.outputStream || process.stdout;
   var files = [];
   var transform = function(file, encoding, cb) {
@@ -24,7 +25,13 @@ var gulpTape = function(opts) {
   };
   var flush = function(cb) {
     try {
-      tape.createStream().pipe(outputStream);
+      if (outputStream === process.stdout) {
+        tape.createStream().pipe(outputStream);
+      } else if (typeof outputStream === 'function') {
+        tape.createStream().pipe(outputStream()).pipe(process.stdout);
+      } else {
+        tape.createStream().pipe(outputStream).pipe(process.stdout);
+      }
       forEach(files, function(file) {
         requireUncached(file);
       });
