b237f7a329a9443c24e6076ffdd34db79ed4e882
Yuan Qing <yuanqing.lim@honestbee.com>
2015-09-02 09:27:14 +0800

    Use a `reporter`

--- test/gulpfile.js
+++ test/gulpfile.js
@@ -2,8 +2,11 @@
 
 var gulp = require('gulp');
 var tape = require('../');
+var tapColorize = require('tap-colorize');
 
 gulp.task('default', function() {
   return gulp.src('fixtures/*.js')
-    .pipe(tape());
+    .pipe(tape({
+      reporter: tapColorize()
+    }));
 });
