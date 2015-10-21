ff392c1304bf36735c6faf573ffeaac672a65b1a
Mario Nebl <mario.nebl@sinnerschrader.com>
2015-10-21 22:52:51 +0200

    refactor: use child_process.spawn instead of require-uncached

--- index.js
+++ index.js
@@ -1,54 +1,39 @@
 'use strict';
 
-var tape = require('tape');
+var spawn = require('child_process').spawn;
 var through = require('through2');
 var PluginError = require('gulp-util').PluginError;
-var requireUncached = require('require-uncached');
 
-var PLUGIN_NAME = 'gulp-tape';
+function createError(payload) {
+  return new PluginError('gulp-tape', payload);
+}
 
-var gulpTape = function(opts) {
+function gulpTape(opts) {
   opts = opts || {};
 
   var outputStream = opts.outputStream || process.stdout;
   var reporter     = opts.reporter     || through.obj();
-  var files        = [];
 
-  var transform = function(file, encoding, cb) {
+  return through.obj(function(file, encoding, cb) {
     if (file.isNull()) {
       return cb(null, file);
     }
     if (file.isStream()) {
-      return cb(new PluginError(PLUGIN_NAME, 'Streaming is not supported'));
+      return cb(createError('Streaming is not supported'));
     }
-    files.push(file.path);
-    cb(null, file);
-  };
 
-  var flush = function(cb) {
-    try {
-      tape.createStream().pipe(reporter).pipe(outputStream);
-      files.forEach(function(file) {
-        requireUncached(file);
-      });
-      var tests = tape.getHarness()._tests;
-      var pending = tests.length;
-      if (pending === 0) {
-        return cb();
-      }
-      tests.forEach(function(test) {
-        test.once('end', function() {
-          if (--pending === 0) {
-            cb();
-          }
-        });
-      });
-    } catch (err) {
-      cb(new PluginError(PLUGIN_NAME, err));
-    }
-  };
-
-  return through.obj(transform, flush);
-};
+    spawn('node', [file.path])
+      .on('error', function(err){
+        cb(createError(err));
+      })
+      .on('exit', function(code){
+        if (code !== 0) {
+          return cb(createError(file.path + ' exited with non-zero code: ' + code));
+        }
+        cb();
+      })
+      .stdout.pipe(reporter).pipe(outputStream);
+  });
+}
 
 module.exports = gulpTape;
