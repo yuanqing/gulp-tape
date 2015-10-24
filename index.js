93abadf0f130afed6c63910ba0acb790ca01aa37
Mario Nebl <mario.nebl@sinnerschrader.com>
2015-10-24 22:16:10 +0200

    feature: run tests in forked child_processes, adapt coverage api

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
 'use strict';
 
-var spawn = require('child_process').spawn;
+var fork = require('child_process').fork;
+var path = require('path');
 var through = require('through2');
+var merge = require('lodash.merge');
 var PluginError = require('gulp-util').PluginError;
+var entry = path.resolve(__dirname, './fork.js');
 
 function createError(payload) {
   return new PluginError('gulp-tape', payload);
+}
+
+function getTestRunner(reporter, outputStream, map, callback) {
+  return through.obj(function(file, encoding, cb) {
+    if (file.isNull()) {
+      return cb(null, file);
+    }
+
+    if (file.isStream()) {
+      return cb(createError('Streaming is not supported'));
+    }
+
+    fork(entry, [file.contents.toString(), file.path, JSON.stringify(map || {})], {
+      silent: true
+    }).on('error', function(err){
+        console.error(err);
+        cb(createError(err));
+      })
+      .on('exit', function(code){
+        if (code !== 0) {
+          return cb(createError(file.path + ' exited with non-zero code: ' + code));
+        }
+        cb(null, file);
+      })
+      .on('message', function(payload){
+        merge(global, payload);
+      }).stdout.pipe(reporter).pipe(outputStream);
+  }, callback);
 }
 
 function gulpTape(opts) {
@@ -13,27 +44,22 @@ function gulpTape(opts) {
 
   var outputStream = opts.outputStream || process.stdout;
   var reporter     = opts.reporter     || through.obj();
+  var exec         = opts.exec;
 
-  return through.obj(function(file, encoding, cb) {
-    if (file.isNull()) {
-      return cb(null, file);
-    }
-    if (file.isStream()) {
-      return cb(createError('Streaming is not supported'));
-    }
+  if (!exec) {
+    return getTestRunner(reporter, outputStream);
+  } else {
+    var map = {};
 
-    spawn('node', [file.path])
-      .on('error', function(err){
-        cb(createError(err));
-      })
-      .on('exit', function(code){
-        if (code !== 0) {
-          return cb(createError(file.path + ' exited with non-zero code: ' + code));
-        }
-        cb();
-      })
-      .stdout.pipe(reporter).pipe(outputStream);
-  });
+    return through.obj(function(file, encoding, cb){
+      map[file.path] = file.contents.toString();
+      cb(null, file);
+    }, function(callback){
+      exec.pipe(getTestRunner(reporter, outputStream, map, function(){
+        callback();
+      })).on('error', callback);
+    });
+  }
 }
 
 module.exports = gulpTape;
--- test/gulpfile.js
+++ test/gulpfile.js
@@ -13,27 +13,38 @@ gulp.task('test', function() {
     }));
 });
 
+'use strict';
+
+var gulp = require('gulp');
+var istanbul = require('gulp-istanbul');
+var tapColorize = require('tap-colorize');
+
+var tape = require('../');
+
+gulp.task('test', function() {
+  return gulp.src('fixtures/test/*.js')
+    .pipe(tape({
+      reporter: tapColorize()
+    }));
+});
+
 gulp.task('istanbul', function(cb) {
-  gulp.src('fixtures/*.js')
+  return gulp.src('fixtures/*.js')
     .pipe(istanbul())
-    .pipe(istanbul.hookRequire())
-    .on('finish', function() {
-      gulp.src('fixtures/test/*.js')
-        .on('error', cb)
-        .on('end', cb)
-        .pipe(tape())
-        .pipe(istanbul.writeReports({
-          reporters: ['lcov', 'text']
-        }))
-        .pipe(istanbul.enforceThresholds({
-          thresholds: {
-            global: {
-              statements: 83,
-              functions: 90,
-              branches: 75,
-              lines: 83,
-            },
-          },
-        }));
-    });
+    .pipe(tape({
+      'exec': gulp.src('fixtures/test/*.js')
+    }))
+    .pipe(istanbul.writeReports({
+      reporters: ['lcov', 'text']
+    }))
+    .pipe(istanbul.enforceThresholds({
+      thresholds: {
+        global: {
+          statements: 83,
+          functions: 90,
+          branches: 75,
+          lines: 83
+        }
+      }
+    }));
 });
