495760ac3b5611bf428a5e76d9a4aff425c169e2
Veeck <veeck@neofonie.de>
2018-03-21 17:56:25 +0100

    Update with resolve-bin

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const childProcess = require('child_process')
 const dargs = require('dargs')
-const path = require('path')
 const PluginError = require('plugin-error')
+const resolveBin = require('resolve-bin')
 const TapParser = require('tap-parser')
 const through = require('through2')
 
 const PLUGIN_NAME = 'gulp-tape'
-const TAPE_BINARY_FILEPATH = path.resolve(__dirname, '../.bin/tape')
+const TAPE_BINARY_FILEPATH = resolveBin.sync('tape')
 
 function gulpTape (options) {
   options = options || {}
