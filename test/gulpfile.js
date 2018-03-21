af00dddcc39ba51775c6825978e3bf9e8ecb22d6
Veeck <veeck@neofonie.de>
2018-03-21 12:12:10 +0100

    Try to fix tests

--- test/gulpfile.js
+++ test/gulpfile.js
@@ -1,11 +1,10 @@
 const gulp = require('gulp')
 const log = require('fancy-log')
-
-const tape = require('../')
+const tape = require('tape')
 
 gulp.task('test', function () {
   return gulp
-    .src('fixtures/test.js')
+    .src('./fixtures/test.js')
     .pipe(
       tape({
         bail: true
